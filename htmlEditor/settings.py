7a6add5e7d700b9e22b647e59e1997c3b979d77f
Jaskaran Singh <sjaskaran10@yandex.com>
2019-09-19 13:46:14 +0530

    Allowed hosts in settings

--- htmlEditor/settings.py
+++ htmlEditor/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h)i@z5rw*rq%p97&i^iwqg+n9=^t@325ig1$^*8=ks##bqnwq8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
